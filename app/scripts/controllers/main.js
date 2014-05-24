de74b4694cdadca1ddf4d71efd1ff3f6c3baadae
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2014-05-24 11:05:14 +0900

    step-1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('helloAngularApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.world = 'Angular';
   });
